e43af6a191078c8a4d4eab385373be5dc972033d
sasedev <sasedev.bifidis@gmail.com>
2016-01-30 04:21:22 +0100

    Call to a member function getParameter() on a non-object (container)

--- src/Sasedev/Commons/SharedBundle/Controller/BaseController.php
+++ src/Sasedev/Commons/SharedBundle/Controller/BaseController.php
@@ -29,7 +29,6 @@ use Symfony\Component\Validator\Validator;
  */
 class BaseController implements ContainerAwareInterface
 {
-
 	use ContainerAwareTrait;
 
 	/**
@@ -77,7 +76,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function has($id)
 	{
+
 		return $this->container->has($id);
+
 	}
 
 	/**
@@ -90,11 +91,13 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function get($id)
 	{
+
 		if (!$this->has($id)) {
 			throw new \LogicException('The service ' . $id . ' is not installed in your application.');
 		}
 
 		return $this->container->get($id);
+
 	}
 
 	/**
@@ -106,11 +109,13 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getParameter($name)
 	{
-		try {
-			return $this->container->getParameter($name);
-		} catch (\Exception $e) {
-			return null;
+
+		$parameter = null;
+		if (null != $this->container) {
+			$parameter = $this->container->getParameter($name);
 		}
+		return $parameter;
+
 	}
 
 	/**
@@ -120,7 +125,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getLogger()
 	{
+
 		return $this->get('logger');
+
 	}
 
 	/**
@@ -130,7 +137,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getMailer()
 	{
+
 		return $this->get('mailer');
+
 	}
 
 	/**
@@ -140,7 +149,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getRouter()
 	{
+
 		return $this->get('router');
+
 	}
 
 	/**
@@ -150,7 +161,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getRequest()
 	{
+
 		return $this->get('request_stack')->getCurrentRequest();
+
 	}
 
 	/**
@@ -160,7 +173,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getSession()
 	{
+
 		return $this->get('session');
+
 	}
 
 	/**
@@ -170,7 +185,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getDoctrine()
 	{
+
 		return $this->get('doctrine');
+
 	}
 
 	/**
@@ -180,7 +197,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getTranslator()
 	{
+
 		return $this->get('translator');
+
 	}
 
 	/**
@@ -190,7 +209,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getSecurityTokenStorage()
 	{
+
 		return $this->get('security.token_storage');
+
 	}
 
 	/**
@@ -200,7 +221,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getSecurityAuthorizationChecker()
 	{
+
 		return $this->get('security.authorization_checker');
+
 	}
 
 	/**
@@ -210,7 +233,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getFormFactory()
 	{
+
 		return $this->get('form.factory');
+
 	}
 
 	/**
@@ -220,7 +245,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getValidator()
 	{
+
 		return $this->get('validator');
+
 	}
 
 	/**
@@ -230,7 +257,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getTemplating()
 	{
+
 		return $this->get('templating');
+
 	}
 
 	/**
@@ -244,7 +273,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function generateUrl($route, $parameters = array(), $absolute = false)
 	{
+
 		return $this->getRouter()->generate($route, $parameters, $absolute);
+
 	}
 
 	/**
@@ -254,7 +285,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getReferer()
 	{
+
 		return $this->getRequest()->headers->get('referer');
+
 	}
 
 	/**
@@ -268,10 +301,12 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function forward($controller, $path = array(), $query = array())
 	{
+
 		$path['_controller'] = $controller;
 		$subRequest = $this->getRequest()->duplicate($query, null, $path);
 
 		return $this->getHttpKernel()->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+
 	}
 
 	/**
@@ -284,7 +319,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function redirect($url, $status = 302)
 	{
+
 		return new RedirectResponse($url, $status);
+
 	}
 
 	/**
@@ -295,9 +332,11 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function flashMsgSession($type, $msg)
 	{
+
 		$this->getSession()
 			->getFlashBag()
 			->set($type, $msg);
+
 	}
 
 	/**
@@ -307,12 +346,14 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function getEntityManager($name = null)
 	{
+
 		$entityManager = $this->getDoctrine()->getManager($name);
 		if (!$entityManager->isOpen()) {
 			$entityManager = $entityManager->create($entityManager->getConnection(), $entityManager->getConfiguration());
 		}
 
 		return $entityManager;
+
 	}
 
 	/**
@@ -327,7 +368,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function translate($id, $parameters = array(), $domain = 'messages', $locale = null)
 	{
+
 		return $this->getTranslator()->trans($id, $parameters, $domain, $locale);
+
 	}
 
 	/**
@@ -342,7 +385,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function hasRole($rolename)
 	{
+
 		return $this->get('security.authorization_checker')->isGranted($rolename);
+
 	}
 
 	/**
@@ -356,7 +401,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function createFormBuilder($formName, $data = null, $options = array())
 	{
+
 		return $this->getFormFactory()->createBuilder($formName, $data, $options);
+
 	}
 
 	/**
@@ -370,7 +417,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function createForm($type, $data = null, $options = array())
 	{
+
 		return $this->getFormFactory()->create($type, $data, $options);
+
 	}
 
 	/**
@@ -383,7 +432,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function renderView($view, $parameters = array())
 	{
+
 		return $this->getTemplating()->render($view, $parameters);
+
 	}
 
 	/**
@@ -397,7 +448,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function renderResponse($view, $parameters = array(), Response $response = null)
 	{
+
 		return $this->getTemplating()->renderResponse($view, $parameters, $response);
+
 	}
 
 	/**
@@ -411,7 +464,9 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function sendmail($message)
 	{
+
 		return $this->getMailer()->send($message);
+
 	}
 
 	/**
@@ -424,6 +479,7 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function endswith($string, $suffix)
 	{
+
 		$strlen = strlen($string);
 		$testlen = strlen($suffix);
 		if ($testlen > $strlen) {
@@ -431,6 +487,7 @@ class BaseController implements ContainerAwareInterface
 		}
 
 		return substr_compare($string, $suffix, -$testlen) === 0;
+
 	}
 
 	/**
@@ -442,16 +499,84 @@ class BaseController implements ContainerAwareInterface
 	 */
 	public function normalize($string)
 	{
-		$table = array('Š' => 'S', 'š' => 's', 'Ð' => 'Dj', 'Ž' => 'Z', 'ž' => 'z', 'C' => 'C', 'c' => 'c', 'C' => 'C', 'c' => 'c',
-			'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
-			'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O',
-			'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss',
-			'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e',
-			'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o',
-			'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b',
-			'ÿ' => 'y', 'R' => 'R', 'r' => 'r');
+
+		$table = array(
+			'Š' => 'S',
+			'š' => 's',
+			'Ð' => 'Dj',
+			'Ž' => 'Z',
+			'ž' => 'z',
+			'C' => 'C',
+			'c' => 'c',
+			'C' => 'C',
+			'c' => 'c',
+			'À' => 'A',
+			'Á' => 'A',
+			'Â' => 'A',
+			'Ã' => 'A',
+			'Ä' => 'A',
+			'Å' => 'A',
+			'Æ' => 'A',
+			'Ç' => 'C',
+			'È' => 'E',
+			'É' => 'E',
+			'Ê' => 'E',
+			'Ë' => 'E',
+			'Ì' => 'I',
+			'Í' => 'I',
+			'Î' => 'I',
+			'Ï' => 'I',
+			'Ñ' => 'N',
+			'Ò' => 'O',
+			'Ó' => 'O',
+			'Ô' => 'O',
+			'Õ' => 'O',
+			'Ö' => 'O',
+			'Ø' => 'O',
+			'Ù' => 'U',
+			'Ú' => 'U',
+			'Û' => 'U',
+			'Ü' => 'U',
+			'Ý' => 'Y',
+			'Þ' => 'B',
+			'ß' => 'Ss',
+			'à' => 'a',
+			'á' => 'a',
+			'â' => 'a',
+			'ã' => 'a',
+			'ä' => 'a',
+			'å' => 'a',
+			'æ' => 'a',
+			'ç' => 'c',
+			'è' => 'e',
+			'é' => 'e',
+			'ê' => 'e',
+			'ë' => 'e',
+			'ì' => 'i',
+			'í' => 'i',
+			'î' => 'i',
+			'ï' => 'i',
+			'ð' => 'o',
+			'ñ' => 'n',
+			'ò' => 'o',
+			'ó' => 'o',
+			'ô' => 'o',
+			'õ' => 'o',
+			'ö' => 'o',
+			'ø' => 'o',
+			'ù' => 'u',
+			'ú' => 'u',
+			'û' => 'u',
+			'ý' => 'y',
+			'ý' => 'y',
+			'þ' => 'b',
+			'ÿ' => 'y',
+			'R' => 'R',
+			'r' => 'r'
+		);
 
 		return strtr($string, $table);
+
 	}
 
 	/**
